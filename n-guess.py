15a8e65a3fcd48d852850117400a441f95d38907
Deepthi-1111 <nagadeepthibodempudi@gmail.com>
2025-12-07 22:38:32 +0530

    Update n-guess.py

--- n-guess.py
+++ n-guess.py
@@ -1,22 +1,3 @@
-# import random
-
-# n = random.randint(1,100)
-# a = int(input())
-# # print('your guess :',a)
-# if a < n :
-#     print("too low")
-# elif a > n:
-#     print("high")
-# elif a == n:
-#     print("correct")
-# else:
-#     print('invalid')
-    
-
-
-
-
-
 import random
 
 n = random.randint(1,100)
@@ -33,4 +14,5 @@ while True :
             break
     except ValueError:
         print('invalid')
+
     
