1288f4fcc328596108369de4dc94048d79a1ca8d
Bodempudi Naga Deepthi <nagadeepthibodempudi@gmail.com>
2025-12-17 22:52:07 +0530

    Update RockPaperScissor.py

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,50 +1,44 @@
 import random
 
-ROCK = 'r'
-SCISSORS = 's'
-PAPER = 'p'
-emojis = { ROCK: '🪨', SCISSORS: '✂️', PAPER: '📃' }
-choices = tuple(emojis.keys())
+e = {'r':'🪨','p' : '📃','s':'✂️'}
 
+while True:
+    
+    user = input('enter the input :').lower()
+    if user != ('r' or 'p' or 's') :
+        print("invalid input , try again ")
 
-def get_user_choice():
-  while True:
-    user_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
-    if user_choice in choices:
-      return user_choice      
+    comp = random.choice(('r','p','s'))
+    print(f'computer choose ',{e[comp]})
+    if user == 'r':
+        if comp =='r':
+            print("same ")
+        elif comp =='p':
+            print("you lose")
+        elif comp == 's':
+            print("you win")
+
+    if user == 'p':
+        if comp =='p':
+            print("same ")
+        elif comp =='s':
+            print("you lose")
+        elif comp == 'r':
+            print("you win")
+        break
+
+    if user == 's':
+        if comp =='s':
+            print("same ")
+        elif comp =='r':
+            print("you lose")
+        elif comp == 'p':
+            print("you win")
+
+    b = input('y/n : ')
+    if b == 'n':
+        break
     else:
-      print('Invalid choice!')
+        continue
 
-
-def display_choices(user_choice, computer_choice):
-  print(f'You chose {emojis[user_choice]}')
-  print(f'Computer chose {emojis[computer_choice]}')
-
-
-def determine_winner(user_choice, computer_choice):
-  if user_choice == computer_choice:
-    print('Tie!')
-  elif (
-    (user_choice == ROCK and computer_choice == SCISSORS) or 
-    (user_choice == SCISSORS and computer_choice == PAPER) or 
-    (user_choice == PAPER and computer_choice == ROCK)):
-    print('You win')
-  else:
-    print('You lose')  
-
-
-def play_game():
-  while True:
-    user_choice = get_user_choice()
-    computer_choice = random.choice(choices)
-    display_choices(user_choice, computer_choice)
-    determine_winner(user_choice, computer_choice)
-    should_continue = input('Continue? (y/n): ').lower()
-    if should_continue == 'n':
-      break
-      
-play_game()
-
-
-
-
+    break
