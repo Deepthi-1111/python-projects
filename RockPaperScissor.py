7be21ad278ee1eff71002939d750f16499d3aaa4
Bodempudi Naga Deepthi <nagadeepthibodempudi@gmail.com>
2025-12-18 22:41:44 +0530

    Update RockPaperScissor.py

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -5,40 +5,43 @@ e = {'r':'🪨','p' : '📃','s':'✂️'}
 while True:
     
     user = input('enter the input :').lower()
-    if user != ('r' or 'p' or 's') :
+    if user not in ('r' , 'p' , 's') :
         print("invalid input , try again ")
-
+        continue                #continue so that the input q repeats itself
     comp = random.choice(('r','p','s'))
-    print(f'computer choose ',{e[comp]})
-    if user == 'r':
-        if comp =='r':
-            print("same ")
-        elif comp =='p':
-            print("you lose")
-        elif comp == 's':
-            print("you win")
 
-    if user == 'p':
-        if comp =='p':
-            print("same ")
-        elif comp =='s':
-            print("you lose")
-        elif comp == 'r':
-            print("you win")
-        break
+    print('you chose :',e[user])
+    print('computer choose :',e[comp])
 
-    if user == 's':
-        if comp =='s':
-            print("same ")
-        elif comp =='r':
-            print("you lose")
-        elif comp == 'p':
-            print("you win")
+    if user == comp:
+        print("same")
+    elif (
+        (user == 'r' and comp == 's') or # here / can be written to move to next line as well instead of ( )  
+        (user == 'p' and comp == 'r') or
+        (user == 's' and comp == 'p') ):
+        print('you win')
+    else:
+        print('you lose')
 
-    b = input('y/n : ')
+    # if user == 'r':
+    #     elif comp =='p':
+    #         print("you lose")
+    #     elif comp == 's':
+    #         print("you win")
+    # if user == 'p':
+    #     elif comp =='s':
+    #         print("you lose")
+    #     elif comp == 'r':
+    #         print("you win")
+    # if user == 's':
+    #     elif comp =='r':
+    #         print("you lose")
+    #     elif comp == 'p':
+    #         print("you win")
+
+#correct but long format
+    
+    b = input('y/n : ').lower()
     if b == 'n':
         break
-    else:
-        continue
-
-    break
+#no need for continue if b = yes as it is done automatically 
