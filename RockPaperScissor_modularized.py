57a9449f69b9feb7c832f16dd1ff0795e99df3d0
Bodempudi Naga Deepthi <nagadeepthibodempudi@gmail.com>
2025-12-22 22:39:31 +0530

    Update RockPaperScissor_modularized.py

--- RockPaperScissor_modularized.py
+++ RockPaperScissor_modularized.py
@@ -1,7 +1,5 @@
 #the popular refactoring technique : modularization applied to rock ,paper ,scissor code 
 
-
-
 import random
 
 e = {'r':'🪨','p' : '📃','s':'✂️'}
@@ -13,27 +11,25 @@ def user_choice ():
             continue
         else :
             return user                
-  
-while True:
-    user_choice()
-                  
-    comp = random.choice(('r','p','s'))
-
-    print('you chose :',e[user])
-    print('computer choose :',e[comp])
 
 def play(user,comp):
-        if user == comp:
-            print("same")
-        elif (
-            (user == 'r' and comp == 's') or 
-            (user == 'p' and comp == 'r') or
-            (user == 's' and comp == 'p') ):
-            print('you win')
-        else:
-            print('you lose')
+    if user == comp:
+        print("same")
+    elif (
+        (user == 'r' and comp == 's') or 
+        (user == 'p' and comp == 'r') or
+        (user == 's' and comp == 'p') ):
+        print('you win')
+    else:
+        print('you lose')
 
+while True:
+    user_global = user_choice()  # user is declared inside function so this step is to make it global      
+    comp = random.choice(('r','p','s'))
+    print('you chose :',e[user_global])
+    print('computer choose :',e[comp])
 
+    play(user_global ,comp )
 
     b = input('y/n : ').lower()
     if b == 'n':
